0859dbd9ea0d77c98470754b2d83e559e76205f0
Shubham Goyal <goyalshubham.2811@gmail.com>
2025-07-02 12:11:11 +0530

    new commit

---
+++ Vision-Parking/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+
+@pytest.fixture(scope="function")
+def driver():
+    options = UiAutomator2Options()
+    options.platform_name = 'Android'
+    options.automation_name = 'UiAutomator2'
+    options.device_name = 'Android Emulator'
+    options.app_package = 'com.example.visionpark'
+    options.app_activity = 'com.example.visionpark.activities.SplashScreenActivity'
+    options.implicit_wait_timeout = 10000
+    options.set_capability('uiautomator2ServerInstallTimeout', 60000)
+    options.no_reset = False
+
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', options=options)
+    yield driver
+    driver.quit()
